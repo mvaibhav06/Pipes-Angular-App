8cf363e2f6f09548444446e59ce0bcf256e1b43c
vaibhav <vaibhavmasetty06@gmail.com>
2024-03-09 21:54:28 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+  };
+
+  miles: number;
+
+  onNameChange(val: string) {
+    this.name = val;
+  }
+
+  onDateChange(val: string) {
+    this.date = val;
+  }
+
+  onAmountChange(val: string) {
+    this.amount = parseFloat(val);
+  }
+
+  onHeightChange(val: string) {
+    this.height = parseFloat(val);
+  }
+
+  onMilesChange(val: string) {
+    this.miles = parseFloat(val);
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+})
+export class ConvertPipe implements PipeTransform {
+  transform(value: any, targetUnits: string): any {
+    if (!value) {
+      return '';
+    }
+
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.609;
+
+      case 'm':
+        return value * 1.609 * 1000;
+
+      default:
+        return value * 1.609;
+    }
+
+    return value * 1.60934;
+  }
+}
